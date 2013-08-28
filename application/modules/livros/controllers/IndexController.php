3fede89d6249b395a825ecf633cb87eba6c2fbef
Rodrigo Machado <rodrigo.aocubo@gmail.com>
2013-08-27 21:35:55 -0300

    livros, livro, route

--- application/modules/livros/controllers/IndexController.php
+++ application/modules/livros/controllers/IndexController.php
@@ -1,8 +1,17 @@
 <?php
 class Livros_IndexController extends Zend_Controller_Action
 {
+
+    public function postDispatch()
+    {
+        if (isset($this->view->pageTitle)) {
+            $this->_helper->layout()->getView()->headTitle($this->view->pageTitle);
+        }
+    }
+
     public function init()
     {
+//        $this->db = Zend_Registry::get('db');
         $layoutHelper = $this->_helper->getHelper('Layout');
         $layout = $layoutHelper->getLayoutInstance();
         $layout->nestedLayout = 'inner_livros';
@@ -10,18 +19,19 @@ class Livros_IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-
         $pageData = array(
-            'leftSpecialUri' => 'menina-bonita-do-laco-de-fita',
+            'leftSpecialUri' => '/explore/historia/menina-bonita-do-laco-de-fita',
             'leftSpecialTitle' => 'Menina bonita do laço de fita',
             'leftSpecialSummary' => 'História de uma menina que, de tão bonita, deixa até um coelho apaixonado.',
-            'rightSpecialUri' => 'a-audacia-dessa-mulher',
+            'leftSpecialImageUri' => '/img/marcacao_destaque_livro1.png',
+            'rightSpecialUri' => '/explore/historia/a-audacia-dessa-mulher',
             'rightSpecialTitle' => 'A audácia dessa mulher',
             'rightSpecialSummary' => 'Um romance que engloba varias vertentes e vem do século XIX aos nossos dias.',
-
+            'rightSpecialImageUri' => '/img/marcacao_destaque_livro2.png',
         );
 
         $this->view->pageData = $pageData;
+        $this->view->pageTitle = "Ana Maria Machado - Histórias";
 
     }
 }
