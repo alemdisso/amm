8d143ed533a053d55755b44a655fceb7775090ac
Rodrigo Machado <rodrigo.machado@alemdisso.com.br>
2014-08-28 16:29:00 -0300

    admin category create

---
+++ application/modules/admin/controllers/CategoryController.php
@@ -0,0 +1,60 @@
+<?php
+
+class Admin_CategoryController extends Zend_Controller_Action
+{
+    private $db;
+    private $workMapper;
+
+    public function preDispatch()
+    {
+        try {
+            $checker = new Moxca_Access_PrivilegeChecker();
+        } catch (Exception $e) {
+            throw $e;
+        }
+        $this->view->pageTitle = "";
+    }
+
+    public function init()
+    {
+        $this->db = Zend_Registry::get('db');
+
+        $layoutHelper = $this->_helper->getHelper('Layout');
+        $this->view->setNestedLayout($layoutHelper, 'inner_admin');
+
+        $this->workMapper = new Author_Collection_WorkMapper($this->db);
+        $this->prizeMapper = new Author_Collection_PrizeMapper($this->db);
+    }
+
+   public function createAction()
+    {
+        // cria form
+        $form = new Moxca_Form_CategoryCreate;
+        $this->view->form = $form;
+
+        if ($this->getRequest()->isPost()) {
+            $postData = $this->getRequest()->getPost();
+            if ($form->isValid($postData)) {
+                $newCategory = $form->process($postData);
+                $this->_helper->getHelper('FlashMessenger')
+                    ->addMessage($this->view->translate('#The record was successfully updated.'));
+                $this->_redirect('/admin/index/list-posts');
+            } else {
+                //form error: populate and go back
+                $form->populate($postData);
+                $this->view->form = $form;
+            }
+        } else {
+
+            $data = $this->_request->getParams();
+            try {
+                $workId = $this->view->checkIdFromGet($data, 'work');
+            } catch (Exception $e) {
+                throw $e;
+            }
+
+        }
+    }
+
+
+}
